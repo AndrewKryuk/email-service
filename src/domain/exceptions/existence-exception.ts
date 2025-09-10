5eb5f3352e7b741d2bbbd7ff4b3b358b84a696c2
AndrewKryuk <me@kryuk.pro>
2025-09-10 16:00:15 +0300

    delete domain exceptions

--- src/domain/exceptions/existence-exception.ts
+++
@@ -1,8 +0,0 @@
-import { ConflictException } from '@kryuk/ddd-kit/application/exceptions/domain/conflict-exception';
-import { ERROR_CODES } from '@domain/constants/error-codes';
-
-export class ExistenceException extends ConflictException {
-  constructor(message: string) {
-    super([{ message, code: ERROR_CODES.EXISTENCE }]);
-  }
-}
--- src/domain/exceptions/not-found-exception.ts
+++
@@ -1,8 +0,0 @@
-import { NotFoundException as BaseNotFoundException } from '@kryuk/ddd-kit/application/exceptions/domain/not-found-exception';
-import { ERROR_CODES } from '@domain/constants/error-codes';
-
-export class NotFoundException extends BaseNotFoundException {
-  constructor(message: string) {
-    super([{ message, code: ERROR_CODES.NOT_FOUND }]);
-  }
-}
